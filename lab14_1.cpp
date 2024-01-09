8a2a6ef7d161b6c4cbb2df4fea6f89f8af57dfcc
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2024-01-10 06:16:17 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,9 +3,24 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    int i,j;
+    T key;
+    for (i = 1; i < N; i++) {
+        key = d[i];
+        j = i - 1;
+         while (j >= 0 && d[j] < key) {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+			d[j + 1] = key;
+			cout << "Pass " << i << ":";
+			for(int s = 0; s < 10; s++){
+				 cout << d[s] << " ";
+			}
+			cout << "\n" ;
+    }
+	
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
